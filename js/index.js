27cf4cc53650bc4901aae3b234e545927a7e3d2f
MenardDevClass <menard.maxime94@gmail.com>
2021-12-27 13:11:07 +0100

    modification des listes, ajout commentaires

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 let list = new List();
 
-FileList.hydrate(recipes);
-FileList.display(list.all);
+list.hydrate(recipes); // introduit les données
+list.display(list.all); // affichage de la liste
 
 let ingredientFilter = new FilterByIngredient();
 let applianceFilter = new FilterByAppliance();
 let ustensilFilter = new FilterByUstensils();
 
-list.addFilter(ingredientFilter);
-list.addFilter(applianceFilter);
-list.addfilter(ustensilFilter);
+list.addFilter(ingredientFilter); // ajoute le filtre ingredients
+list.addFilter(applianceFilter); // ajoute le filtre appliance
+list.addFilter(ustensilFilter); // ajoute le filtre ustensils
 
 list.search = new Search();
-list.search.listen();
+list.search.listen(); // filtre pour la recherche input

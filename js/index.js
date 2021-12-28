541a9fbd6793e1efcc5f645fcadaf43cebeefc47
MenardDevClass <menard.maxime94@gmail.com>
2021-12-28 14:44:38 +0100

    harmonisation commentaires sur l'index JS

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 let list = new List();
 
-list.hydrate(recipes); // introduit les données
-list.display(list.all); // affichage de la liste
+list.hydrate(recipes); // introducing data
+list.display(list.all); // displays the list
 
 let ingredientFilter = new FilterByIngredient();
 let applianceFilter = new FilterByAppliance();
 let ustensilFilter = new FilterByUstensils();
 
-list.addFilter(ingredientFilter); // ajoute le filtre ingredients
-list.addFilter(applianceFilter); // ajoute le filtre appliance
-list.addFilter(ustensilFilter); // ajoute le filtre ustensils
+list.addFilter(ingredientFilter); // adds ingredients filter
+list.addFilter(applianceFilter); // adds appliance filter
+list.addFilter(ustensilFilter); // adds ustensils filter
 
 list.search = new Search();
-list.search.listen(); // filtre pour la recherche input
+list.search.listen(); // filter for input search

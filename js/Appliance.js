b0d554de750a7047a8101c2efbd22fa8222b0503
MenardDevClass <menard.maxime94@gmail.com>
2021-12-31 15:32:37 +0100

    Ajout de "Appliance"  récupère les données des appareils, et ajoute filtre appareils dans les recettes

---
+++ js/Appliance.js
@@ -0,0 +1,32 @@
+class FilterByAppliance extends Filters
+{
+    constructor()
+    {
+        super('appliance');
+    }
+
+    //-- Récupère la collecte de données appareil
+    collect(recipes)
+    {
+        let list = new Set();
+
+        recipes.forEach(recipe => {
+            list.add(recipe.appliance.toLowerCase());
+        });
+        return list;
+    }
+    
+
+    //-- Filtre : appareils dans les recettes avec l'appareil sélectionné dropdown
+    filter(recipes)
+    {
+        if (this.selected.size === 0) {
+            return recipes;
+        }
+
+        return recipes.filter(recipe => 
+        {
+            return !!this.selected.has(recipe.appliance.toLowerCase());
+        })
+    }
+}

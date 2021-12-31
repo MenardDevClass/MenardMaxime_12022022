31e9a4a2697f26fc26cfb5426e3106005c5a62a6
MenardDevClass <menard.maxime94@gmail.com>
2021-12-31 15:44:15 +0100

    ajout ustensils : filtres dans les recettes (dropdown)

--- js/Ustensils.js
+++ js/Ustensils.js
@@ -16,3 +16,32 @@ class FilterByIngredient extends Filters
         });
         return list;
     }
+    //-- Filtre : ingredients dans les recettes avec l'ingredient sélectionné dropdown
+    filter(recipes)
+    {
+        if(this.selected.size === 0) {
+            return recipes;
+        }
+
+        return recipes.filter(recipe => 
+        {
+            let isSelectable = false;
+            let count = 0;
+
+            this.selected.forEach(ing => 
+            {
+                let existingIngredients = recipe.ingredients.map(item => item.ingredient.toLowerCase());
+                if(existingIngredients.includes(ing)) 
+                {
+                    count++
+                }  
+            })
+
+            if(count == this.selected.size) 
+            {
+                isSelectable = true;
+            }
+            return isSelectable;
+        })
+    }
+}
